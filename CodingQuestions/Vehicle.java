f732c705ef379b94be99491ff75b638b231b57fa
yzeng059 <91687052+yzeng059@users.noreply.github.com>
2023-12-11 17:16:30 -0800

    Update Vehicle.java

--- CodingQuestions/Vehicle.java
+++ CodingQuestions/Vehicle.java
@@ -1,1 +1,11 @@
+public abstract class Vehicle {
+    private String licensePlate;
 
+    public Vehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+}
