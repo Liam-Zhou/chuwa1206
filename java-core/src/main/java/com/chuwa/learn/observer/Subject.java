7bc71cd616a4b6fa0ca56ee37b9b59036acb8a50
kevinge7 <yuxig5@bu.edu>
2023-12-13 01:48:00 -0500

    hw3

---
+++ java-core/src/main/java/com/chuwa/learn/observer/Subject.java
@@ -0,0 +1,8 @@
+package com.chuwa.learn.observer;
+
+
+public interface Subject {
+    void attach(Observer observer);
+    void detach(Observer observer);
+    void notify(String message);
+}
---
+++ java-core/src/main/java/com/chuwa/learn/observer/SubscriptionSubject.java
@@ -0,0 +1,25 @@
+package com.chuwa.learn.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubscriptionSubject implements Subject {
+
+    private List<Observer> wechatUserList = new ArrayList<Observer>();
+    @Override
+    public void attach(Observer observer) {
+        wechatUserList.add(observer);
+    }
+
+    @Override
+    public void detach(Observer observer) {
+        wechatUserList.remove(observer);
+    }
+
+    @Override
+    public void notify(String message) {
+        for (Observer observer : wechatUserList) {
+            observer.update(message);
+        }
+    }
+}
