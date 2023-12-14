c31a59245ea4272acca996cf49ed02851d199078
Yue Zhuo <81711860+y1zhuo@users.noreply.github.com>
2023-12-13 18:51:41 -0800

    Yue_Zhuo/hw2

---
+++ CodingQuestions/code1/Vehicle.java
@@ -0,0 +1,23 @@
+// Enum representing different types of vehicles
+enum VehicleType {
+    CAR, MOTORCYCLE
+}
+
+// Class representing a Vehicle
+class Vehicle {
+    private String licensePlate;
+    private VehicleType type;
+
+    public Vehicle(String licensePlate, VehicleType type) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+}
