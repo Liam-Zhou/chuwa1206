fed99def23cb26929c7862b2eb8d587ec0135f41
WenqingZhong <zhongwenqing99@gmail.com>
2023-12-17 22:54:21 -0500

    Q2

---
+++ CodingQuestions/hw4/t06_java8/exercise/EmptyCartException.java
@@ -0,0 +1,8 @@
+package com.chuwa.tutorial.t06_java8.exercise;
+
+
+public class EmptyCartException extends RuntimeException {
+    public EmptyCartException(String message) {
+        super(message);
+    }
+}
