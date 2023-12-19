dd52144cae987ed6797de681b36a1de89315544e
Your Name <you@example.com>
2023-12-18 17:02:46 -0800

    hw4

---
+++ CodingQuestions/Practical_java8/Customer.java
@@ -0,0 +1,37 @@
+package Practical_java8;
+
+    public class Customer {
+
+        private Long id;
+
+        private String name;
+        private Integer tier;
+
+        public Customer() {
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getTier() {
+            return tier;
+        }
+
+        public void setTier(Integer tier) {
+            this.tier = tier;
+        }
+    }
+
---
+++ CodingQuestions/t06_java8/features/lambda/Foo.java
@@ -0,0 +1,18 @@
+package t06_java8.features.lambda;
+
+@FunctionalInterface
+public interface Foo {
+    String aMethod(String string);
+
+    default String defaultBar() {
+        String s = "default Bar method";
+        System.out.println(s);
+        return s;
+    }
+
+    default String defaultCommon() {
+        String s = "defaultCommon from Bar";
+        System.out.println(s);
+        return s;
+    }
+}
