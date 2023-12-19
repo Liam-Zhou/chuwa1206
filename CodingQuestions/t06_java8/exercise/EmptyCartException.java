c767796b8cd0948e9cb001a5d096d72d38b6a301
Your Name <you@example.com>
2023-12-18 16:52:42 -0800

    hw4

---
+++ CodingQuestions/t06_java8/exercise/EmptyCartException.java
@@ -0,0 +1,11 @@
+package t06_java8.exercise;
+
+/**
+ * @author b1go
+ * @date 4/12/23 11:37 PM
+ */
+public class EmptyCartException extends RuntimeException {
+    public EmptyCartException(String message) {
+        super(message);
+    }
+}
