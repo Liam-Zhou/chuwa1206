62f13c540500d6d4d6a26b85a56a781e4566cbc8
wx <arieldddwx@gmail.com>
2023-12-23 20:37:25 -0600

    hw5

--- tutorial/javacore/src/main/java/tutorial/excercise/completablefuture/CompletableFutureHw3.java
+++ tutorial/javacore/src/main/java/tutorial/excercise/completablefuture/CompletableFutureHw3.java
@@ -1,21 +1,31 @@
 package tutorial.excercise.completablefuture;
 
-import java.util.concurrent.CompletableFuture;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+// DEFAULT_VALUE
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
-public class CompletableFutureHw2 {
+public class CompletableFutureHw3 {
 
     private static final HttpClient httpClient = HttpClient.newHttpClient();
+    private static final Logger logger = Logger.getLogger(CompletableFutureHw2.class.getName());
+    // DEFAULT_VALUE
+    private static final String DEFAULT_VALUE = "{}";
 
     public static CompletableFuture<String> fetchFromAPI(String endpoint) {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://my-json-server.typicode.com/arield3/FakeAPI/" + endpoint))
                 .build();
         return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body);
+                .thenApply(HttpResponse::body)
+                .exceptionally(e -> {
+                    logger.log(Level.SEVERE, "Exception occurred during API call: " + endpoint, e);
+                    return DEFAULT_VALUE;
+                });
     }
 
     public static void main(String[] args) {
