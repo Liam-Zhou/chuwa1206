ffbab9181a8fd84232e89c71a66ae45be51a0bb4
cookyMonsterKG <kenling@uci.edu>
2024-01-02 14:07:40 -0500

    java thread practice

--- CodingQuestions/chuwa-eij-tutorial/02-java-core/src/main/java/com/chuwa/tutorial/t08_multithreading/c01_creat/MyThread.java
+++ CodingQuestions/chuwa-eij-tutorial/02-java-core/src/main/java/com/chuwa/tutorial/t08_multithreading/c01_creat/MyThread.java
@@ -4,9 +4,16 @@ package com.chuwa.tutorial.t08_multithreading.c01_creat;
  * @author b1go
  * @date 3/21/22 9:02 AM
  */
-public class MyThread extends Thread {
+public class MyThread extends  Thread{
     @Override
-    public void run() {
-        System.out.println("start new thread using extends thread");
+    public void run(){
+
+        try {
+            System.out.println("MyThread runs");
+            sleep(10000);
+            System.out.println("MyThread finishes");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
