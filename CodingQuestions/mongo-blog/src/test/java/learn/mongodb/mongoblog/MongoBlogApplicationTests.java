a32f017a442a86ba8b3b1fc50599b45535137b13
Tangkaiyuan0311 <m18852083652@163.com>
2024-01-07 20:24:56 -0600

    complete mongo-blog

---
+++ CodingQuestions/mongo-blog/src/test/java/learn/mongodb/mongoblog/MongoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.mongodb.mongoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
