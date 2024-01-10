4ae6559787d82e73924ed9cd22b17717bcadac78
Qiushi Zhou <qiushi.zhou@qq.com>
2024-01-09 16:47:16 -0800

    hw7 finish

---
+++ mongo-blog/src/main/java/com/chuwa/mongoblog/payload/BlogDto.java
@@ -0,0 +1,54 @@
+package com.chuwa.mongoblog.payload;
+
+
+public class BlogDto {
+    private String id;
+    private String title;
+    private String description;
+    private String content;
+
+
+    public BlogDto() {
+
+    }
+
+    public BlogDto(String id, String title, String description, String content) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
