a93021b9e3e6459746e672f0d3a36a889dd8c114
Hanyun <hanyunzh@usc.edu>
2024-01-11 01:46:59 -0800

    complete hw7. Mongoblog in /MavenProject

---
+++ MavenProject/HW7 Mongo Blog/mongo-blog/src/main/java/com/chuwa/mongoblog/MongoBlogApplication.java
@@ -0,0 +1,26 @@
+package com.chuwa.mongoblog;
+
+import com.chuwa.mongoblog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class MongoBlogApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBlogApplication.class, args);
+	}
+
+	@Autowired
+	private PostService postService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.postService.createGroceryItems();
+		this.postService.showAllGroceryItems();
+	}
+}
