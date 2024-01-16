f29d30250d3075e1a3e016a70faf512e30b62d66
zyf <zf2023@nyu.edu>
2024-01-16 11:48:09 -0800

    hw11

---
+++ bank.statement/src/main/java/com/example/bank/statement/controller/UserProfileController.java
@@ -0,0 +1,24 @@
+package com.example.bank.statement.controller;
+
+import com.example.bank.statement.payload.UserProfileDto;
+import com.example.bank.statement.service.UserProfileService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user_profile")
+public class UserProfileController {
+    @Autowired
+    private UserProfileService userProfileService;
+    @PostMapping
+    public ResponseEntity<UserProfileDto> createUserProfile(@Valid @RequestBody UserProfileDto userProfileDto) {
+        return new ResponseEntity<>(this.userProfileService.createUserProfile(userProfileDto), HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserProfileDto> getUserProfileById(@PathVariable(name = "id") long id) {
+        return new ResponseEntity<>(this.userProfileService.getUserProfileById(id), HttpStatus.OK);
+    }
+}
---
+++ bank.statement/src/main/java/com/example/bank/statement/entity/Transaction.java
@@ -0,0 +1,33 @@
+package com.example.bank.statement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transaction")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+    @CreationTimestamp
+    private LocalDateTime date;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+}
