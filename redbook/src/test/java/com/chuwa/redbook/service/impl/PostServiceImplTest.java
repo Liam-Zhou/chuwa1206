4ce20ef6a497e61502bcb477d5896dcf5a25d712
Hanyun <hanyunzh@usc.edu>
2024-02-02 00:24:26 -0800

    complete unit test for PostServiceImpl

--- redbook/src/test/java/com/chuwa/redbook/service/impl/PostServiceImplTest.java
+++ redbook/src/test/java/com/chuwa/redbook/service/impl/PostServiceImplTest.java
@@ -13,9 +13,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,6 +39,7 @@ class PostServiceImplTest {
 
     private PostDto postDto;
     private Post post;
+    private final List<Post> allPost = new ArrayList<>();
 
 
     @BeforeAll
@@ -42,16 +48,20 @@ class PostServiceImplTest {
     }
 
     @BeforeEach
-    void setUp() {
+    void generateSinglePost() {
         logger.info("set up Post for each test");
 
         this.post = new Post();
+        post.setId(1L);
         post.setTitle("Unit Test");
-        post.setDescription("2024.1.30");
+        post.setDescription("2024.2.1");
         post.setContent("postServiceImplTest");
         ModelMapper modelMapper = new ModelMapper();
         this.postDto = modelMapper.map(this.post, PostDto.class);
+
+        allPost.add(post);
     }
+
 
     @Test
     void createPost() {
@@ -67,12 +77,61 @@ class PostServiceImplTest {
         Assertions.assertEquals(postDto.getDescription(), postResponse.getDescription());
         Assertions.assertEquals(postDto.getContent(), postResponse.getContent());
     }
-//
-//    @Test
-//    void getAllPost() {
-//    }
-//
-//    @Test
-//    void getPostById() {
-//    }
+
+    @Test
+    void getAllPost() {
+        // define the behavior
+        Mockito.when(postRepositoryMock.findAll()).thenReturn(allPost);
+
+        // execute
+        List<PostDto> response = postService.getAllPost();
+
+        // assertions
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(allPost.size(), response.size());
+    }
+
+    @Test
+    void getPostById() {
+        // define behavior
+        Mockito.when(postRepositoryMock.findById(ArgumentMatchers.anyLong()))
+                .thenThrow(new ResourceNotFoundException("Post", "id", post.getId()))
+                .thenReturn(Optional.ofNullable(post));
+
+        // execute1
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> postService.getPostById(post.getId()));
+
+        // execute2
+        PostDto response = postService.getPostById(post.getId());
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getId(), post.getId());
+        Assertions.assertEquals(response.getTitle(), post.getTitle());
+    }
+
+
+    @Test
+    void updatePost() {
+        // define behavior
+        Mockito.when(postRepositoryMock.save(ArgumentMatchers.any(Post.class))).thenReturn(post);
+
+        Mockito.when(postRepositoryMock.findById(ArgumentMatchers.anyLong()))
+                .thenThrow(new ResourceNotFoundException("Post", "id", post.getId()))
+                .thenReturn(Optional.ofNullable(post));
+
+        // execute1
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> postService.updatePost(postDto, -1));
+
+        // execute2
+        PostDto response = postService.updatePost(postDto, 1);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getId(), post.getId());
+        Assertions.assertEquals(response.getTitle(), post.getTitle());
+    }
+
+    @Test
+    void deletePostById(){
+        postService.deletePostById(1);
+    }
+
 }
